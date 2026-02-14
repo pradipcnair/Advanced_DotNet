71359d94b592386f80a8d91d9cffdb85f000edb9
Pradip C Nair <pradip.c.nair@gmail.com>
2026-02-14 11:31:03 +0530

    StaticConstructor additional

---
+++ StaticConstructor/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace StaticConstuctor
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            StaticConstructor instance1 = new StaticConstructor();
+            instance1.DisplayValue();
+        }
+    }
+    public class StaticConstructor
+    {
+        public static int Value { get; set; }
+        static StaticConstructor()
+        {
+            Value = 42;
+            Console.WriteLine("Static constructor called.");
+        }
+
+        public void DisplayValue()
+        {
+            Console.WriteLine($"Value: {Value}");
+        }
+    }
+}
